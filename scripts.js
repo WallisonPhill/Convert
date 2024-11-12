3a1ee3c52ccd2109427f79889b331591a39c38c6
Wallisonphill <wallisonphill@gmail.com>
2024-11-12 11:15:01 +0100

    Fazendo os calculos, formatando moeda, etc

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 //Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -38,8 +39,21 @@ function convertCurrency(amount, price, symbol) {
 //Exibindo a cotação da moeda selecionada.    
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
     
-    
-    //Aplica a classe que exibe o footer para mostrar o resultado.
+//Calcula o total    
+    let total = amount * price 
+
+//Verifica se o resultado não é um número.    
+    if (isNaN(total)) {
+      return alert("Por favor, digite um valor numérico.")
+    }
+
+//Formata o valor total para real    
+    total = formatCurrencyBRL(total)
+
+//Exibe o resultado total
+    result.textContent = (`${total}`)
+
+//Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
   }
   catch (error){
